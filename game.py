cc41ecd9fd1183ff5714644e0151d01c3155bc2e
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-06-11 00:07:29 +0530

    Setting snake's default direction

--- game.py
+++ game.py
@@ -22,3 +22,6 @@ snake_position = [100, 50]
 snake_body = [  [100, 50],[90, 50],[80, 50],[70, 50]]
 fruit_position = [random.randrange(1, (window_x//10)) * 10,random.randrange(1, (window_y//10)) * 10]
 fruit_spawn = True
+
+direction = 'RIGHT'
+change_to = direction
