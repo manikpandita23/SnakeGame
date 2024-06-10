fadbee7d4e7ed6a9c6ddbc41b93be8dd8b9aaec0
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-06-11 00:12:08 +0530

    Display text

--- game.py
+++ game.py
@@ -32,3 +32,4 @@ def show_score (choice, color, font, size):
     score_font = pygame.font.SysFont(font, size)
     score_surface = score_font.render('Score : ' + str(score), True, color)
     score_rect = score_surface.get_rect()
+    game_window.blit(score_surface, score_rect)
