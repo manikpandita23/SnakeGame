1cbb21a651d0d4033ec194d7a04cd9a42a5f59d9
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-06-12 00:03:41 +0530

    Snake body growing mechanism

--- game.py
+++ game.py
@@ -73,3 +73,20 @@ while True:
             snake_position[0] -= 10
         if direction == 'RIGHT':
             snake_position[0] += 10
+        
+        snake_body.insert(0, list(snake_position))
+        if snake_position[0] == fruit_position[0] and snake_position[1]==fruit_position[1]:
+            score +=10
+            fruit_spawn = False
+        else:
+            snake_body.pop()
+
+        if not fruit_spawn:
+            fruit_position = [random.randrange(1, (window_x//10))*10, random.randrange(1, (window_y//10))*10]
+        fruit_spawn = True
+        game_window.fill(black)
+
+        for pos in snake_body:
+            pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1],10, 10))
+        pygame.draw.rect(game_window, white, pygame.Rect(fruit_position[0], fruit_position[1], 10, 10))
+        
